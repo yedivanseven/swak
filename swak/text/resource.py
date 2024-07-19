e087d1c8f6bf91a3dce3257e88cb6d9df5db46ef
yedivanseven <georg@muckisnspirit.com>
2024-07-19 13:37:18 +0200

    Fixed docstring

--- swak/text/resource.py
+++ swak/text/resource.py
@@ -13,7 +13,7 @@ class TextResourceLoader(ArgRepr):
         Name of the python package under which the text file is located.
     prefix: str, optional
         Directory under which the text file is located within the python
-        package. May contain any number of slashes to access nested
+        package. May contain any number of forward slashes to access nested
         subdirectories. Defaults to "resources".
     encoding: str, optional
         Encoding of the text file. Defaults to "utf-8".
@@ -41,8 +41,8 @@ class TextResourceLoader(ArgRepr):
         *args
             Additional arguments will be interpolated into the joined string
             of `prefix` and `path` by calling its `format` method. Obviously,
-            the number of args must be equal to the total number of
-            placeholders in the combined `prefix` and `path`.
+            the number of args must be equal to (or greater than) the total
+            number of placeholders in the combined `prefix` and `path`.
 
         Returns
         -------
