e74772e6574906a180162f529dae80b4e7e07b2a
yedivanseven <georg@muckisnspirit.com>
2024-10-09 18:34:24 +0200

    Make progress bar disappear after each epoch

--- swak/pt/train/trainer.py
+++ swak/pt/train/trainer.py
@@ -238,7 +238,7 @@ class Trainer(ArgRepr):
         for epoch in range(epoch + 1, self.max_epochs + 1):
             # Train one epoch, looping over batches.
             model.train()
-            data = tqdm(train(self.batch_size), 'Batches', n_batches)
+            data = tqdm(train(self.batch_size), 'Batches', n_batches, False)
             for features, target in data:
                 optimizer.zero_grad()
                 predictions = model(*features)
