3b1349dbbe974af77eaceb5be3b833411e418c02
yedivanseven <yedivanseven@outlook.de>
2025-06-02 22:11:53 +0200

    Added ToDo

--- swak/jsonobject/jsonobject.py
+++ swak/jsonobject/jsonobject.py
@@ -292,6 +292,7 @@ class JsonObject(metaclass=SchemaMeta):
     def __bool__(self) -> bool:
         return bool(self.__dict__)
 
+    # ToDo: Add the or operator overload ("|") as a synonym for __call__
     def __call__(self, mapping: Raw | Self = None, **kwargs: Any) -> Self:
         """Update one or more (nested) fields with `mapping` and kwargs.
 
