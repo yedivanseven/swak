7c2975af7a38eb6edb779a1ae71443a56295ca65
yedivanseven <yedivanseven@outlook.de>
2026-04-14 11:49:31 +0200

    Bumped versions of github actions

--- src/swak/__init__.py
+++ src/swak/__init__.py
@@ -1,5 +1,3 @@
-# ToDo: Transition to src layout
-# ToDo: Check versions in GitHub actions and pip install shpinx!
 # ToDo: Update swak-template
 # - Map over steps
 # - Catch SafeErrors in main (see update-api)
