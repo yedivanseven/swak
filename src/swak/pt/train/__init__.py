e744606799850a398f4c3e615ccec39a108e79a0
yedivanseven <georg@muckisnspirit.com>
2026-05-31 12:17:12 +0200

    Fixed docstring

--- src/swak/pt/train/__init__.py
+++ src/swak/pt/train/__init__.py
@@ -1,10 +1,10 @@
 """Flexibly parameterized and feature-rich training loop for models.
 
 Allows for warm-up periods as well as early stopping with recovery of the best
-checkpoint from CPU memory or disk. Use the provided callback examples to
-monitor training progress or derive your own from the respective base classes.
-Custom templates for train and test data to seamlessly work with the training
-loop are also provided.
+checkpoint from any supported storage option. Use the provided callback
+examples to monitor training progress or derive your own from the respective
+base classes. Templates for train and test data to seamlessly work with
+the training loop are also provided.
 
 """
 
